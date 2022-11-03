04529da40bae6e336318cabb93341b098d164ea6
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2022-11-03 10:31:21 +0100

    fix raspa path on fidis

--- src/script.py
+++ src/script.py
@@ -18,7 +18,7 @@ def fidis(raspa_dir, lib_dir, raspa_bin, **kwargs):
     str_out += "#SBATCH --time 24:00:00\n"
     str_out += "#SBATCH --mem 32G\n\n"
     str_out += "export RASPA_DIR=%s\n" %raspa_dir
-    str_out += "export DYLD_LIBARY=%s\n" %lib_dir
+    str_out += "export DYLD_LIBRARY=%s\n" %lib_dir
     str_out += "export LD_LIBRARY_PATH=%s\n\n" %lib_dir
     str_out += "%s simulation.input\n" %raspa_bin
     return str_out
